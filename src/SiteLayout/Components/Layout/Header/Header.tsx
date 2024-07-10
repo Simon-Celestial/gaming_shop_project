a614ac19b5331abd6dbabbfa9a956edf6fddb243
Ziya Gasimli <gasimli.ziya@yandex.com>
2024-07-11 03:52:27 +0400

    folders structured

---
+++ src/SiteLayout/Components/Layout/Header/Header.tsx
@@ -0,0 +1,8 @@
+import styles from './Header.module.scss';
+export const Header = () => {
+    return (
+        <header className={styles.headerWrapper}>
+            <h1>HEADER</h1>
+        </header>
+    );
+};
