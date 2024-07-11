dcf1eb951e3efb9b5e78fd4257f94822477789e1
Ziya Gasimli <gasimli.ziya@yandex.com>
2024-07-11 20:06:22 +0400

    sideMenu in progress

--- src/SiteLayout/Components/Layout/Header/Header.tsx
+++ src/SiteLayout/Components/Layout/Header/Header.tsx
@@ -6,85 +6,89 @@ import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import ExpandLessOutlinedIcon from '@mui/icons-material/ExpandLessOutlined';
 import {Link} from "react-router-dom";
+import {SideMenu} from "../SideMenu/SideMenu.tsx";
 
 export const Header = () => {
     return (
-        <header className={styles.headerWrapper}>
-            <div className={styles.headerContent}>
-                <div className={styles.leftBlock}>
-                    <Link to={"/"} className={styles.logoBlock}>
-                        <img src="/images/siteLogo.png" alt="Project Logo"/>
-                        <p>game<span>storm</span></p>
-                    </Link>
-                    <div className={styles.phoneBlock}>
-                        <PhoneAndroidSharpIcon/>
-                        <a href="tel:(302) 555-0107" target={"_blank"}>(302) 555-0107</a>
-                    </div>
-                </div>
-                <nav className={styles.navigationBlock}>
-                    <div className={styles.navEntity}>
-                        <Link to={"/"} className={styles.entity}>Home</Link>
-                    </div>
-                    <div className={styles.navEntity}>
-                        <Link to={"/shop"} className={styles.entity}>Shop</Link>
-                    </div>
-                    <div className={styles.navEntity}>
-                        <p className={styles.entity}>Pages<ExpandLessOutlinedIcon/></p>
-                        <div className={styles.navDropDown}>
-                            <div className={styles.dropDownContent}>
-                                <Link to={"/"}>
-                                    Option 1
-                                </Link>
-                                <Link to={"/"}>
-                                    Option 2
-                                </Link>
-                                <Link to={"/"}>
-                                    Option 3
-                                </Link>
-                                <Link to={"/"}>
-                                    Option 4
-                                </Link>
-                                <Link to={"/"}>
-                                    Option 5
-                                </Link>
-                                <Link to={"/"}>
-                                    Option 6
-                                </Link>
-                                <Link to={"/"}>
-                                    Option 7
-                                </Link>
-                                <Link to={"/"}>
-                                    Option 8
-                                </Link>
-                            </div>
-
+        <>
+            <SideMenu/>
+            <header className={styles.headerWrapper}>
+                <div className={styles.headerContent}>
+                    <div className={styles.leftBlock}>
+                        <Link to={"/"} className={styles.logoBlock}>
+                            <img src="/images/siteLogo.png" alt="Project Logo"/>
+                            <p>game<span>storm</span></p>
+                        </Link>
+                        <div className={styles.phoneBlock}>
+                            <PhoneAndroidSharpIcon/>
+                            <a href="tel:(302) 555-0107" target={"_blank"}>(302) 555-0107</a>
                         </div>
                     </div>
-                    <div className={styles.navEntity}>
-                        <Link to={"/"} className={styles.entity}>About</Link>
-                    </div>
-                    <div className={styles.navEntity}>
-                        <Link to={"/"} className={styles.entity}>Blog</Link>
-                    </div>
-                    <div className={styles.navEntity}>
-                        <Link to={"/"} className={styles.entity}>Contact</Link>
-                    </div>
-                </nav>
-                <div className={styles.rightBlock}>
-                    <div className={styles.buttonBlock}>
-                    <SearchSharpIcon/>
-                    </div>
-                    <div className={styles.buttonBlock}>
-                        <FavoriteBorderIcon/>
-                    </div>
-                    <div className={styles.buttonBlock}>
-                        <PersonOutlineIcon/>
-                    </div>
-                    <div className={`${styles.buttonBlock} ${styles.cartButton}`}>
-                        <ShoppingCartOutlinedIcon />
+                    <nav className={styles.navigationBlock}>
+                        <div className={styles.navEntity}>
+                            <Link to={"/"} className={styles.entity}>Home</Link>
+                        </div>
+                        <div className={styles.navEntity}>
+                            <Link to={"/shop"} className={styles.entity}>Shop</Link>
+                        </div>
+                        <div className={styles.navEntity}>
+                            <p className={styles.entity}>Pages<ExpandLessOutlinedIcon/></p>
+                            <div className={styles.navDropDown}>
+                                <div className={styles.dropDownContent}>
+                                    <Link to={"/"}>
+                                        Option 1
+                                    </Link>
+                                    <Link to={"/"}>
+                                        Option 2
+                                    </Link>
+                                    <Link to={"/"}>
+                                        Option 3
+                                    </Link>
+                                    <Link to={"/"}>
+                                        Option 4
+                                    </Link>
+                                    <Link to={"/"}>
+                                        Option 5
+                                    </Link>
+                                    <Link to={"/"}>
+                                        Option 6
+                                    </Link>
+                                    <Link to={"/"}>
+                                        Option 7
+                                    </Link>
+                                    <Link to={"/"}>
+                                        Option 8
+                                    </Link>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className={styles.navEntity}>
+                            <Link to={"/"} className={styles.entity}>About</Link>
+                        </div>
+                        <div className={styles.navEntity}>
+                            <Link to={"/"} className={styles.entity}>Blog</Link>
+                        </div>
+                        <div className={styles.navEntity}>
+                            <Link to={"/"} className={styles.entity}>Contact</Link>
+                        </div>
+                    </nav>
+                    <div className={styles.rightBlock}>
+                        <div className={styles.buttonBlock}>
+                            <SearchSharpIcon/>
+                        </div>
+                        <div className={styles.buttonBlock}>
+                            <FavoriteBorderIcon/>
+                        </div>
+                        <div className={styles.buttonBlock}>
+                            <PersonOutlineIcon/>
+                        </div>
+                        <div className={`${styles.buttonBlock} ${styles.cartButton}`}>
+                            <ShoppingCartOutlinedIcon/>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </>
     );
 };
--- src/SiteLayout/Components/Layout/SideMenu/SideMenu.tsx
+++ src/SiteLayout/Components/Layout/SideMenu/SideMenu.tsx
@@ -1,5 +1,36 @@
+import styles from "./SideMenu.module.scss";
+import WidgetsIcon from '@mui/icons-material/Widgets';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import XIcon from '@mui/icons-material/X';
+import {Link} from "react-router-dom";
+
 export const SideMenu = () => {
     return (
-        <div className={styles}></div>
+        <div className={styles.sideMenuWrapper}>
+            <div className={styles.menuLogo}>
+                <WidgetsIcon />
+                menu
+            </div>
+            <div className={styles.socialBox}>
+                <Link to={"https://www.facebook.com/"} className={styles.iconBox} target={"_blank"}>
+                    <FacebookIcon/>
+                </Link>
+                <Link to={"https://www.instagram.com/"} className={styles.iconBox} target={"_blank"}>
+                    <InstagramIcon/>
+                </Link>
+                <Link to={"https://www.linkedin.com/"} className={styles.iconBox} target={"_blank"}>
+                    <LinkedInIcon/>
+                </Link>
+                <Link to={"https://x.com/"} className={styles.iconBox} target={"_blank"}>
+                    <XIcon/>
+                </Link>
+            </div>
+            <div className={styles.languageBox}>
+                ENG
+            </div>
+
+        </div>
     );
 };
